f4aaf35e59e33034642fabf4b18ace647ccd614a
Bogdan Delic <bogdanworkdelic@gmail.com>
2024-10-03 15:13:41 +0200

    added animations for main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        driveIn: {
+          "0%": { transform: "translateX(-100vw)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        driveIn: "driveIn 2s ease-out forwards",
+        fadeIn: "fadeIn 3s ease-out forwards",
+      },
+    },
   },
   plugins: [],
 };
